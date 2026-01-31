2f9b7e2573eb5aa1b208f7a7dda3547e0d66c133
Jaime Suarez <jaisua8@gmail.com>
2026-01-30 23:41:02 -0500

    creacion de la pagina del login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,43 @@
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
-export default Login
+    <div
+      className="w-full min-h-screen flex justify-center items-center  text-white 
+    bg-[url(/fondo-principal.png)] bg-white/90"
+    >
+      <div className=" p-2 rounded bg-white bg-opacity-75 space-y-3">
+        <div className="w-lg h-64 bg-[url(/fondo-principal.png)] bg-cover bg-center  text-center">
+          <div className="bg-white bg-opacity-50 h-full flex items-center justify-center">
+            <p className="block text-shadow m-auto h-fit shadow-amber-500 bg-linear-to-r from-pink-500 to-violet-950 bg-clip-text text-5xl font-extrabold text-transparent">
+              Bienvenido a tu pagina de diversiones!
+            </p>
+          </div>
+        </div>
+        <div className="space-y-4">
+          <div className="w-fit mx-auto text-2xl px-5 py-3 rounded-3xl font-extrabold bg-green-700 text-white">User Login</div>
+          <form action="" className="space-y-4">
+            <div className="w-fit mx-auto relative">
+              <input type="text" 
+              className="bg-gray-700 font-bold text-3xl w-sm text-white placeholder-gray-500 rounded-lg pl-10 pr-4 py-3 focus:outline-none focus:ring-8 focus:ring-yellow-500 transition-all duration-300"
+              placeholder="Ingrese el Usuario"
+              />
+              <i className="bi bi-person absolute left-1 top-1.5 text-3xl"></i>
+            </div>
+            <div className="w-fit mx-auto relative">
+              <input type="text" 
+              className="bg-gray-700 font-bold text-4 w-sm text-white placeholder-gray-500 rounded-lg pl-10 pr-4 py-3 focus:outline-none focus:ring-8 focus:ring-yellow-500 transition-all duration-300"
+              placeholder="Ingrese el Usuario"
+              />
+              
+              <i className="bi bi-lock absolute left-1 top-1.5 text-3xl"></i>
+            </div>
+
+            <button className="hover:scale-105 duration-150 w-fit mx-auto block text-2xl px-5 py-3 rounded-2 font-extrabold bg-gray-950 text-white"> Login</button>
+
+          </form>
+          <p className="text-gray-300 py-3 font-serif text-center italic  bg-blue-800 ">Demo User: jaime Password:admin</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Login;
