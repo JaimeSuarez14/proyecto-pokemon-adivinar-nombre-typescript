41663c16d79d530efadc33673025b7b09a068eb9
Jaime Suarez <jaisua8@gmail.com>
2026-01-31 00:09:47 -0500

    link adivina pokemon

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,10 @@
+import { Link } from "react-router"
+
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>Dashboard
+      <Link to={"game_pokemon"}>Jugar Adivina Pokemon</Link>
+    </div>
   )
 }
 export default Dashboard
